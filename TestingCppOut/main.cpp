ada0c2478006eab23b7008bd6a7b997d03a61b21
ramzy2000 <caseyduffy00@gmail.com>
2021-07-01 22:51:47 -0500

    Trying out inheritance

--- TestingCppOut/main.cpp
+++ TestingCppOut/main.cpp
@@ -1,9 +1,66 @@
-#include "VoidPointerTester.h"
+#include <iostream>
+#include <string>
+
+enum class ItemID
+{
+	Apple,
+	Strawberry
+};
+
+class Items
+{
+public:
+	ItemID item;
+	int itemSize;
+
+	ItemID getItemID()
+	{
+		return item;
+	}
+
+	std::string getItemString()
+	{
+		switch (item)
+		{
+		case ItemID::Apple:
+			return "Apple";
+		default:
+			std::cout << "???" << '\n';
+			return "???";
+		}
+	}
+};
+
+class Apple : public Items
+{
+	
+public:
+	std::string itemString;
+	ItemID itemID;
+	Apple()
+		:itemString(getItemString()), itemID(ItemID::Apple)
+
+	{
+		setupID();
+		setupString();
+	}
+
+	
+private:
+	void setupID()
+	{
+		item = ItemID::Apple;
+	}
+
+	void setupString()
+	{
+		itemString = getItemString();
+	}
+};
 
 int main()
 {
-	VoidPointerTester tester;
-	tester.test();
-
+	Apple apple;
+	std::cout << static_cast<int>(apple.getItemID()) << '\n';
 	return 0;
 }
