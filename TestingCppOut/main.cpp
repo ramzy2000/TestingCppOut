c8751268e0c01c1714862ae4f27a7176e1de4d0f
ramzy2000 <caseyduffy00@gmail.com>
2021-08-02 20:48:30 -0500

    Updated to the lasted project i want to work on

--- TestingCppOut/main.cpp
+++ TestingCppOut/main.cpp
@@ -1,66 +1,24 @@
 #include <iostream>
-#include <string>
-
-enum class ItemID
-{
-	Apple,
-	Strawberry
-};
-
-class Items
-{
-public:
-	ItemID item;
-	int itemSize;
-
-	ItemID getItemID()
-	{
-		return item;
-	}
-
-	std::string getItemString()
-	{
-		switch (item)
-		{
-		case ItemID::Apple:
-			return "Apple";
-		default:
-			std::cout << "???" << '\n';
-			return "???";
-		}
-	}
-};
-
-class Apple : public Items
-{
-	
-public:
-	std::string itemString;
-	ItemID itemID;
-	Apple()
-		:itemString(getItemString()), itemID(ItemID::Apple)
-
-	{
-		setupID();
-		setupString();
-	}
-
-	
-private:
-	void setupID()
-	{
-		item = ItemID::Apple;
-	}
-
-	void setupString()
-	{
-		itemString = getItemString();
-	}
-};
+#include <vector>
 
 int main()
 {
-	Apple apple;
-	std::cout << static_cast<int>(apple.getItemID()) << '\n';
+	std::vector<int> numbers{ 8, 10, 40, 33, 67 };
+	std::size_t size{ numbers.size() };
+	int* begin{ numbers.data() };
+	int* end{ numbers.data() + size };
+
+	// for loop
+	std::cout << "First print" << '\n';
+	for (auto* index{ begin }; index != end; ++index)
+	{
+		std::cout << index << ' ';
+	}
+	std::cout << '\n' << "Second print" << '\n';
+	for (int element : numbers)
+	{
+		std::cout << element << ' ';
+	}
+
 	return 0;
 }
